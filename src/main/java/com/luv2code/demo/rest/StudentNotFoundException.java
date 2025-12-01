508fc9b0ca702cba8dd7aee8bb0df863edac3c81
Titus Lim <tituslhy@gmail.com>
2025-12-01 22:51:19 +0800

    add exception handling

---
+++ src/main/java/com/luv2code/demo/rest/StudentNotFoundException.java
@@ -0,0 +1,18 @@
+package com.luv2code.demo.rest;
+
+/**
+ * Generate constructor from the super class
+ */
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +13,52 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" to return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    /**
+     * Initialize the list of students only once to be called
+     * by all the endpoints
+     */
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students" to return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
     }
+
+    /**
+     * Ensure that the variable studentId matches the parameter
+     * name in the method
+     * @param studentId
+     * @return
+     */
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        if (studentId >= theStudents.size() || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+
+    /**
+     * Add an exception handler to catch StudentNotFoundException
+     * @param exc
+     * @return ResponseEntity<StudentErrorResponse
+     */
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
 }
